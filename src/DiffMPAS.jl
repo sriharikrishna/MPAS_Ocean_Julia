a1b4212924c61ddf05a08ad0cdb0bd09b0cbb123
Michel Schanen <mschanen@anl.gov>
2024-04-04 15:32:19 -0500

    Restructure repo for differentiation

---
+++ src/DiffMPAS.jl
@@ -0,0 +1,10 @@
+module DiffMPAS
+include("mode_init/MPAS_Ocean.jl")
+include("mode_forward/calculate_tendencies.jl")
+include("mode_forward/time_steppers.jl")
+#include(CODE_ROOT * "visualization.jl")
+include("mode_init/exactsolutions.jl")
+
+export MPAS_Ocean, kelvinWaveExactSolution!, kevlinWaveExactSSH, kelvinWaveExactNormalVelocity, kelvinWaveGenerator, seed, kelvin_test
+export forward_backward_step!
+end # module DiffMPAS
---
+++ src/mode_forward/time_steppers.jl
@@ -0,0 +1,23 @@
+
+######## KernelAbstractions versions of time-advancing  methods
+
+
+function forward_backward_step!(mpasOcean)
+    calculate_normal_velocity_tendency!(mpasOcean)
+
+    update_normal_velocity_by_tendency!(mpasOcean)
+
+    calculate_thickness_tendency!(mpasOcean)
+
+    update_thickness_by_tendency!(mpasOcean)
+end
+
+function forward_euler_step!(mpasOcean)
+    calculate_normal_velocity_tendency!(mpasOcean)
+
+    calculate_thickness_tendency!(mpasOcean)
+
+    update_normal_velocity_by_tendency!(mpasOcean)
+
+    update_thickness_by_tendency!(mpasOcean)
+end
